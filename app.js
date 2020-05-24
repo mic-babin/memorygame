561ae4efc86a1d86565e6a66b2ad7e71f501075c
mic-babin <61392281+mic-babin@users.noreply.github.com>
2020-05-24 14:10:03 -0400

    lll

--- app.js
+++ app.js
@@ -119,7 +119,7 @@ class MixOrMatch {
           card1.classList.remove('visible');
           card2.classList.remove('visible');
           this.busy = false;
-      }, 2000);
+      }, 1000);
   }
   shuffleCards(cardsArray) {
       for (let i = cardsArray.length - 1; i > 0; i--) {
